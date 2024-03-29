1f7b2a45724d82a20ffa90e094642e53e601b18c
BrokenTime9 <111900146+BrokenTime9@users.noreply.github.com>
2024-03-29 22:31:14 +0530

    Update not-found.jsx

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,10 +3,7 @@ import { useParams } from "next/navigation";
 import React from "react";
 
 export default function NotFound({ side }) {
-<<<<<<< Updated upstream
   return <div>{}Ja na</div>;
-=======
   const { siteName } = useParams();
   return <div> {siteName} Ja na</div>;
->>>>>>> Stashed changes
 }
