256ec8f8cf54ba81240986ab8f541d478fa87125
BrokenTime9 <111900146+BrokenTime9@users.noreply.github.com>
2024-05-10 21:17:28 +0530

    Update index.js

--- src/data/sports/Maharashtra/index.js
+++ src/data/sports/Maharashtra/index.js
@@ -1,77 +1,6 @@
 const Maharashtra = {
   1: {
     id: 1,
-<<<<<<< Updated upstream
-    name: "Sky diving 1",
-    location: "kamshet pune",
-    price: "3000-4500",
-    locationDetailed:
-      "Kamshet is a small town located in the Pune district of Maharashtra, approximately 110 kilometers (68 miles) from Mumbai and 45 kilometers (28 miles) from Pune. It is situated in the Western Ghats, providing a picturesque landscape for paragliding.",
-    season:
-      "The paragliding season in Kamshet typically runs from October to June, with the winter months (November to February) being the peak season. During this time, the weather conditions are more stable and favorable for paragliding.",
-    weather:
-      "The weather conditions play a crucial role in paragliding. Pilots and operators closely monitor wind conditions, thermals, and other factors to ensure safe and enjoyable flights.",
-  },
-  2: {
-    id: 2,
-    name: "Sky diving 2",
-    place: "asdfasdfasdf",
-  },
-  3: {
-    id: 3,
-    name: "Rock Climbing 1",
-    place: "asdfasdfasdf",
-  },
-  4: {
-    id: 4,
-    name: "Rock Climbing 2",
-    place: "asdfasdfasdf",
-  },
-  5: {
-    id: 5,
-    name: "Paragliding 1",
-    place: "asdfasdfasdf",
-  },
-  6: {
-    id: 6,
-    name: "Paragliding 2",
-    place: "asdfasdfasdf",
-  },
-  7: {
-    id: 7,
-    name: "White Water Rafting 1",
-    place: "asdfasdfasdf",
-  },
-  8: {
-    id: 8,
-    name: "Trekking 1",
-    place: "asdfasdfasdf",
-  },
-  9: {
-    id: 9,
-    name: "Trekking 2",
-    place: "asdfasdfasdf",
-  },
-  10: {
-    id: 10,
-    name: "Hot Air Ballooning 1",
-    place: "asdfasdfasdf",
-  },
-  11: {
-    id: 11,
-    name: "Hot Air Ballooning 2",
-    place: "asdfasdfasdf",
-  },
-  12: {
-    id: 12,
-    name: "Jet Skiing 1",
-    place: "asdfasdfasdf",
-  },
-  13: {
-    id: 13,
-    name: "Jet Skiing 1",
-    place: "asdfasdfasdf",
-=======
     name: "Skydiving",
     location: "Kamshet",
     price: "₹3000-₹4500",
@@ -189,7 +118,6 @@ const Maharashtra = {
       "The Sahyadri Ranges, also known as the Western Ghats, stretch across several states in India, including Maharashtra. Trekking in the Sahyadri Ranges offers enthusiasts a chance to explore diverse landscapes, dense forests, and scenic trails, with options ranging from easy walks to challenging hikes.",
     bestWeather: "Winter",
     bestSeason: "October to March",
->>>>>>> Stashed changes
   },
 };
 
